cb14564abfa8cb713c03deafd0e68b177c369cb4
tabjula <lk544219@dal.ca>
2023-02-10 19:07:23 -0400

    chore: Added backend project setup

---
+++ BackEnd/PetRadar/src/main/java/com/Group1/PetRadar/Controller/MainController.java
@@ -0,0 +1,26 @@
+package com.Group1.PetRadar.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/main")
+@CrossOrigin
+public class MainController {
+
+    @GetMapping("/getSomething/{id}")
+    public String getSomething(@PathVariable("id") int id) {
+        return "dummy";
+    }
+
+    @PostMapping("/postSomething")
+    public String postSomething() {
+
+        return "dummy";
+    }
+
+    @DeleteMapping("deleteSomething/{id}")
+    public String deleteMovieById(@PathVariable("id") int id) {
+
+        return "dummy";
+    }
+}
---
+++ BackEnd/PetRadar/src/test/java/com/Group1/PetRadar/PetRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group1.PetRadar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
