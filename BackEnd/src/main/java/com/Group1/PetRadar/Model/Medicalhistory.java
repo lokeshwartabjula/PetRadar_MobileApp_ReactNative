bd116ab2c5aefa8a4e969e4487d83cfc04538266
parshva <pr371441@dal.ca>
2023-02-24 19:41:52 -0400

    Created Medical history model

---
+++ BackEnd/src/main/java/com/Group1/PetRadar/Model/Medicalhistory.java
@@ -0,0 +1,81 @@
+package com.Group1.PetRadar.Model;
+import java.util.Date;
+import jakarta.persistence.Entity;
+import org.springframework.stereotype.Component;
+
+@Component;
+public class Medicalhistory {
+    int medicalHistoryId;
+
+    Date vetVisitDate;
+
+    String symptoms;
+
+    String vetName;
+
+    Date vaccinationDate;
+
+    String surgery;
+
+    String medication;
+
+    public int getMedicalHistoryId() {
+        return medicalHistoryId;
+    }
+
+    public void setMedicalHistoryId(int medicalHistoryId) {
+        this.medicalHistoryId = medicalHistoryId;
+    }
+
+    public Date getVetVisitDate() {
+        return vetVisitDate;
+    }
+
+    public void setVetVisitDate(Date vetVisitDate) {
+        this.vetVisitDate = vetVisitDate;
+    }
+
+    public String getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(String symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public String getVetName() {
+        return vetName;
+    }
+
+    public void setVetName(String vetName) {
+        this.vetName = vetName;
+    }
+
+    public Date getVaccinationDate() {
+        return vaccinationDate;
+    }
+
+    public void setVaccinationDate(Date vaccinationDate) {
+        this.vaccinationDate = vaccinationDate;
+    }
+
+    public String getSurgery() {
+        return surgery;
+    }
+
+    public void setSurgery(String surgery) {
+        this.surgery = surgery;
+    }
+
+    public String getMedication() {
+        return medication;
+    }
+
+    public void setMedication(String medication) {
+        this.medication = medication;
+    }
+
+
+
+
+}
