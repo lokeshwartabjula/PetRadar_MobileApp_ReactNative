031aad2ceb56baf9bd8f7c4c8b9c9b8ca0a2771c
Sankeerth Rani <sn501304@dal.ca>
2023-02-24 18:59:05 -0400

    Created Image Model

---
+++ BackEnd/src/main/java/com/Group1/PetRadar/Model/Image.java
@@ -0,0 +1,39 @@
+package com.Group1.PetRadar.Model;
+
+import jakarta.persistence.Entity;
+import org.springframework.stereotype.Component;
+import java.util.*;
+
+@Component
+public class Image {
+
+    int imageId;
+    String imageLink;
+
+    String imageType;
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    public void setImageLink(String imageLink) {
+        this.imageLink = imageLink;
+    }
+
+    public String getImageType() {
+        return imageType;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+
+
+}
