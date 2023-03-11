9183f9908d0a103f03f93400bcead03ec43de5d9
Dhruvin Dankhara <dh793203@dal.ca>
2023-03-11 18:31:51 -0400

    Refactor: user IMPL according to register user DTO

--- BackEnd/src/main/java/com/Group1/PetRadar/Service/PetProfileService.java
+++ BackEnd/src/main/java/com/Group1/PetRadar/Service/PetProfileService.java
@@ -1,12 +1,16 @@
 package com.Group1.PetRadar.Service;
 
+import com.Group1.PetRadar.DTO.AddPetDTO;
 import com.Group1.PetRadar.Model.PetprofileModel;
 
 public interface PetProfileService {
-    //String saveMovie(Movie movie);
-    PetprofileModel savePetProfile(PetprofileModel petprofileModel);
+    // String saveMovie(Movie movie);
+    PetprofileModel savePetProfile(AddPetDTO petprofileModel);
+
     PetprofileModel getPetprofileById(int id);
+
     String updatePetprofile(PetprofileModel petprofileModel);
+
     String deletePetprofileById(int id);
 
 }
