3b55f8d25bad7d71ed7c957727ae60ad9fe503e6
Dhruvin Dankhara <dh793203@dal.ca>
2023-03-15 23:19:10 -0300

    Add: Include UserID in login and registration API response

--- BackEnd/src/main/java/com/Group1/PetRadar/Controller/UserController.java
+++ BackEnd/src/main/java/com/Group1/PetRadar/Controller/UserController.java
@@ -15,32 +15,13 @@ import com.Group1.PetRadar.Model.User;
 import com.Group1.PetRadar.Service.UserService;
 import com.Group1.PetRadar.protocol.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
-	// @Autowired
-	// UserService userService;
-	//
-	// @PostMapping("/register")
-	// public UserModel registerUser(@RequestBody UserModel user) throws Exception {
-	//
-	// return userService.registerUser(user);
-	// }
-	//
-	// @PostMapping("/login")
-	// public AuthRespDTO loginUser(@RequestBody AuthReqDTO authDto) {
-	//
-	//
-	// return null;
-	//
-	// }
-	//
-	// @PostMapping({ "/hello" })
-	// public String sampleMethod() {
-	// return "Hello World";
-	// }
-
 	@Autowired
 	UserService userService;
 
@@ -53,53 +34,44 @@ public class UserController {
 	@PostMapping("/googleLogin")
 	public ResponseEntity<Response> googleRegisterLogin(@RequestBody User user) {
 		String token = userService.generateToken(user.getEmail());
-
 		Response response = new Response(token, HttpStatus.ACCEPTED.value(), HttpStatus.ACCEPTED.name());
 		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
 	}
 
 	@PostMapping("/register")
 	public ResponseEntity<Response> saveUser(@RequestBody RegisterUserDTO registerUserDTO) {
-
-		String token = userService.generateToken(registerUserDTO.getEmail());
 		User newUser = new User(
 				registerUserDTO.getEmail(),
 				registerUserDTO.getFirstname(),
 				registerUserDTO.getLastname(),
 				registerUserDTO.getProfileurl(),
 				registerUserDTO.getPassword());
-		userService.saveUser(newUser);
-		Response response = new Response(token, HttpStatus.ACCEPTED.value(), HttpStatus.ACCEPTED.name());
+		newUser = userService.saveUser(newUser);
+		String token = userService.generateToken(newUser.getUserId().toString());
+		Response response = new Response();
+		Map<String,String> data = new HashMap<>();
+		data.put("token",token);
+		data.put("userId",newUser.getUserId().toString());
+
+		response.setData(data);
+		response.setMessage(HttpStatus.ACCEPTED.name());
+		response.setStatus(HttpStatus.ACCEPTED.value());
 		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
 	}
 
 	@PostMapping("/login")
 	public ResponseEntity<Response> login(@RequestBody AuthReqDTO authReqDTO) {
-		// System.out.print(payload.toString());
-		// TODO: Check with database and fetch user details
-		// TODO: pass that user object to generateToken method and get the token
-		String token = userService.generateToken(authReqDTO.getEmail());
 
-		Response response = new Response(token, HttpStatus.ACCEPTED.value(), HttpStatus.ACCEPTED.name());
-		System.out.println(userService.getUserEmail("user3@email.com"));
+		UUID userId = userService.getUserEmail(authReqDTO.getEmail());
+		String token = userService.generateToken(userId.toString());
+		Response response = new Response();
+		Map<String,String> data = new HashMap<>();
+		data.put("token",token);
+		data.put("userId",userId.toString());
+
+		response.setData(data);
+		response.setMessage(HttpStatus.ACCEPTED.name());
+		response.setStatus(HttpStatus.ACCEPTED.value());
 		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
 	}
-
-	// @GetMapping("/login")
-	// public ResponseEntity<Response> token(Authentication authentication) {
-	// Response response = new
-	// Response(userService.generateToken(authentication),HttpStatus.ACCEPTED.value(),
-	// HttpStatus.ACCEPTED.name());
-	// return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
-	// }
-
-	// @GetMapping("/me/{userName}")
-	// public ResponseEntity<Response> me(@PathVariable("userName") String userName)
-	// {
-	// Response response = new
-	// Response(userService.getUserId(userName),HttpStatus.OK.value(),
-	// HttpStatus.OK.name());
-	// return ResponseEntity.status(HttpStatus.OK).body(response);
-	// }
-
 }
