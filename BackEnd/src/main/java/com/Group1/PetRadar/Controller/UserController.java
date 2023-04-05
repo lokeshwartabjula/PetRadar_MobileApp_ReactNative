ebb72b444f7e89d07c8d60d8b21eb92bb8081eeb
Dhruvin Dankhara <dh793203@dal.ca>
2023-04-05 15:49:04 -0300

    Feat: Add API for fetching all pets based on user ID

--- BackEnd/src/main/java/com/Group1/PetRadar/Controller/UserController.java
+++ BackEnd/src/main/java/com/Group1/PetRadar/Controller/UserController.java
@@ -1,20 +1,30 @@
 package com.Group1.PetRadar.Controller;
 
-import com.Group1.PetRadar.DTO.auth.AuthReqDTO;
-import com.Group1.PetRadar.DTO.user.RegisterUserDTO;
-import com.Group1.PetRadar.DTO.user.updateUserDTO;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.Group1.PetRadar.DTO.auth.AuthReqDTO;
+import com.Group1.PetRadar.DTO.user.updateUserDTO;
+import com.Group1.PetRadar.Model.PetprofileModel;
 import com.Group1.PetRadar.Model.User;
 import com.Group1.PetRadar.Service.UserService;
 import com.Group1.PetRadar.protocol.Response;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -26,6 +36,28 @@ public class UserController {
 	@GetMapping("/message")
 	public String home() {
 		return "Never give up!!";
+	}
+
+	@GetMapping("/allPet")
+	public ResponseEntity<Response> getAllPetsByUserId(@RequestParam(name = "userId") String id) {
+
+		System.out.println(id);
+		Response failureResponse = null;
+		List<PetprofileModel> pets = null;
+		try {
+			pets = userService.findPetsByUserId(UUID.fromString(id));
+		} catch (Exception e) {
+			failureResponse = new Response(e.getMessage(), HttpStatus.UNAUTHORIZED.value(),
+					HttpStatus.UNAUTHORIZED.name());
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(failureResponse);
+		}
+		Map<String, Object> data = new HashMap<>();
+		data.put("pets", pets);
+		Response response = new Response();
+		response.setData(data);
+		response.setMessage(HttpStatus.ACCEPTED.name());
+		response.setStatus(HttpStatus.ACCEPTED.value());
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
 	}
 
 	@GetMapping("/{id}")
