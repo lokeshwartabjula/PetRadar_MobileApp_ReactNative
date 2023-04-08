8fe13f3411222d37b7389796205ef0563554adab
JEET MEHTA <jt429386@dal.ca>
2023-04-08 06:15:29 -0300

    Bug: Resolved OneSignal Issue

--- BackEnd/src/main/java/com/Group1/PetRadar/Controller/UserController.java
+++ BackEnd/src/main/java/com/Group1/PetRadar/Controller/UserController.java
@@ -217,7 +217,8 @@ public class UserController {
 			@RequestParam("userId") String userId,
 			@RequestParam(name = "image", required = false) MultipartFile file,
 			@RequestParam("latitude") String latitude,
-			@RequestParam("longitude") String longitude) {
+			@RequestParam("longitude") String longitude,
+			@RequestParam("oneSignalUserId") String oneSignalUserId) {
 
 		System.out.println(firstName + lastName + address + city + pincode + mobileNumber + userId);
 		// System.out.println(file.getOriginalFilename());
@@ -232,6 +233,7 @@ public class UserController {
 		updatedUserDetails.setPhoneNumber(Long.parseLong(mobileNumber));
 		updatedUserDetails.setLatitude(new BigDecimal(latitude));
 		updatedUserDetails.setLongitude(new BigDecimal(longitude));
+		updatedUserDetails.setOneSignalUserId(oneSignalUserId);
 
 		Response failureResponse = null;
 		User user = null;
