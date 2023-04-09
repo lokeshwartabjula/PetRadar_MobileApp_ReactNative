8bedbb3f6a4e0990c0396f9489bce63c67efaf5d
JEET MEHTA <jt429386@dal.ca>
2023-04-09 16:23:14 -0300

    fix: finally completed nots issue

--- BackEnd/src/main/java/com/Group1/PetRadar/Service/Implementation/PostServiceImplementation.java
+++ BackEnd/src/main/java/com/Group1/PetRadar/Service/Implementation/PostServiceImplementation.java
@@ -49,7 +49,8 @@ public class PostServiceImplementation implements PostService {
                 System.out.println(
                         user.getOnesignalUserId() + " " + currentUserId.toString() + " " + user.getUserId().toString());
                 System.out.println("usernid ==>" + (currentUserId.toString().equals(user.getUserId().toString())));
-                if (user.getOnesignalUserId() != null && !(currentUserId.toString().equals(user.getUserId())))
+                if (user.getOnesignalUserId() != null
+                        && !(currentUserId.toString().equals(user.getUserId().toString())))
                     userOSID.add(user.getOnesignalUserId());
             });
             try {
