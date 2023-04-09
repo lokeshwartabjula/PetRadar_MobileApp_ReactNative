e574f5e17e755601b69be05f0bf32564268f921e
JEET MEHTA <jt429386@dal.ca>
2023-04-09 00:32:04 -0300

    test: added unit test cases user register

---
+++ BackEnd/src/test/java/com/Group1/PetRadar/Service/UserServiceImplTest.java
@@ -0,0 +1,57 @@
+package com.Group1.PetRadar.Service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.Group1.PetRadar.DTO.user.RegisterUserDTO;
+import com.Group1.PetRadar.Model.User;
+import com.Group1.PetRadar.Repository.UserRepository;
+import com.Group1.PetRadar.Service.Implementation.UserServiceImpl;
+
+public class UserServiceImplTest {
+    @Mock
+    PasswordEncoder passwordEncoderMock;
+
+    @Mock
+    UserRepository userRepoMock;
+
+    @InjectMocks
+    UserServiceImpl userServiceImpl;
+
+    @Test
+    public void saveUserTestPositive1() {
+        User dummyUser = new User();
+        dummyUser.setCity("dummyCity");
+        RegisterUserDTO dummyRegisterUserDTO = new RegisterUserDTO();
+        dummyRegisterUserDTO.setPassword("dummyPWD");
+        when(passwordEncoderMock.encode(any(CharSequence.class))).thenReturn("dummypwdString");
+
+        when(userRepoMock.save(any(User.class))).thenReturn(dummyUser);
+        Assert.assertEquals(dummyUser, userServiceImpl.saveUser(dummyRegisterUserDTO));
+
+    }
+
+    @Test
+    public void saveGoogleUserTest() {
+        User dummyUser = new User();
+        when(userRepoMock.save(any(User.class))).thenReturn(dummyUser);
+        Assert.assertEquals(dummyUser, userServiceImpl.saveGoogleUser(dummyUser));
+    }
+
+    @Test
+    public void saveUserTest() {
+        User dummyUser = new User();
+        dummyUser.setPassword("dummyPass");
+        when(passwordEncoderMock.encode(any(CharSequence.class))).thenReturn("dummypwdString");
+
+        when(userRepoMock.save(any(User.class))).thenReturn(dummyUser);
+        Assert.assertEquals(dummyUser, userServiceImpl.saveUser(dummyUser));
+
+    }
+}
