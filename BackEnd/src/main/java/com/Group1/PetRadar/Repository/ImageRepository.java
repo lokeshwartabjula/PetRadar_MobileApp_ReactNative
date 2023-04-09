bd32a4ac4398adcf4905857919b55c845b297f82
Dhruvin Dankhara <dh793203@dal.ca>
2023-04-09 05:24:50 -0300

    Remove: Image repository file

--- BackEnd/src/main/java/com/Group1/PetRadar/Repository/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.Group1.PetRadar.Repository;
-//
-//import com.Group1.PetRadar.Model.Image;
-//import com.Group1.PetRadar.Model.PetprofileModel;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface ImageRepository extends CrudRepository<Image,Integer> {
-//}
