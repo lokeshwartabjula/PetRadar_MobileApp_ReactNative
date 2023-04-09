eec040df9e969b549a631810999334c73396d622
Dhruvin Dankhara <dh793203@dal.ca>
2023-04-09 18:40:30 -0300

    Refactor: Insufficient modularization smell for updateUserDTO

--- BackEnd/src/main/java/com/Group1/PetRadar/DTO/user/updateUserDTO.java
+++ BackEnd/src/main/java/com/Group1/PetRadar/DTO/user/updateUserDTO.java
@@ -1,7 +1,5 @@
 package com.Group1.PetRadar.DTO.user;
 
-import java.math.BigDecimal;
-
 import org.springframework.web.multipart.MultipartFile;
 
 import com.Group1.PetRadar.Model.Location;
@@ -15,8 +13,6 @@ public class updateUserDTO extends Location {
     private String Pincode;
     private Long mobileNumber;
     private MultipartFile file;
-    private BigDecimal latitude;
-    private BigDecimal longitude;
     private String oneSignalUserId;
 
     public String getFirstName() {
@@ -73,22 +69,6 @@ public class updateUserDTO extends Location {
 
     public void setFile(MultipartFile file) {
         this.file = file;
-    }
-
-    public BigDecimal getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(BigDecimal latitude) {
-        this.latitude = latitude;
-    }
-
-    public BigDecimal getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(BigDecimal longitude) {
-        this.longitude = longitude;
     }
 
     public String getOneSignalUserId() {
