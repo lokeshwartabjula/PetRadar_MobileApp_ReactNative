58bbad69d27668d9c318422541fbc8a0fc4fa0df
Dhruvin Dankhara <dh793203@dal.ca>
2023-04-09 19:33:49 -0300

    Refactor: used lombok annotation for Getter and Setter

--- BackEnd/src/main/java/com/Group1/PetRadar/DTO/user/updateUserDTO.java
+++ BackEnd/src/main/java/com/Group1/PetRadar/DTO/user/updateUserDTO.java
@@ -1,9 +1,16 @@
 package com.Group1.PetRadar.DTO.user;
 
+import java.math.BigDecimal;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import com.Group1.PetRadar.Model.Location;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class updateUserDTO extends Location {
 
     private String firstName;
@@ -11,72 +18,8 @@ public class updateUserDTO extends Location {
     private String address;
     private String city;
     private String Pincode;
-    private Long mobileNumber;
+    private Long phoneNumber;
     private MultipartFile file;
     private String oneSignalUserId;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getPincode() {
-        return Pincode;
-    }
-
-    public void setPincode(String pincode) {
-        Pincode = pincode;
-    }
-
-    public Long getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setPhoneNumber(Long mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public MultipartFile getFile() {
-        return file;
-    }
-
-    public void setFile(MultipartFile file) {
-        this.file = file;
-    }
-
-    public String getOneSignalUserId() {
-        return oneSignalUserId;
-    }
-
-    public void setOneSignalUserId(String oneSignalUserId) {
-        this.oneSignalUserId = oneSignalUserId;
-    }
 
 }
