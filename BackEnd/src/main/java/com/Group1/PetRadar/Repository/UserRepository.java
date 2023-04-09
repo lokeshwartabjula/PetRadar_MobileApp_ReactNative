0b043cb902b3673e4b4defd96777431d61f762e7
parshva <pr371441@dal.ca>
2023-04-09 03:17:31 -0300

    Refactor: Argument data type to big decimal

--- BackEnd/src/main/java/com/Group1/PetRadar/Repository/UserRepository.java
+++ BackEnd/src/main/java/com/Group1/PetRadar/Repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.Group1.PetRadar.Repository;
 
+import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
@@ -13,6 +14,6 @@ public interface UserRepository extends CrudRepository<User, UUID> {
     Optional<User> findByEmail(String username);
 
     @Query(value = "SELECT * FROM user u WHERE (6371 * acos(cos(radians(?1)) * cos(radians(u.latitude)) * cos(radians(u.longitude) - radians(?2)) + sin(radians(?1)) * sin(radians(u.latitude)))) < 20", nativeQuery = true)
-    Collection<User> findUserByLocation(double latitude, double longitude);
+    Collection<User> findUserByLocation(BigDecimal bigDecimal, BigDecimal bigDecimal2);
 
 }
