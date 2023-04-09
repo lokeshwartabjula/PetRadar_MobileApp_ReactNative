41e5c9f277e2242be1a258cd07cb62bbfef74671
Sankeerth Rani <sn501304@dal.ca>
2023-04-08 21:18:41 -0300

    Add: Function getmedicalByPetId  into interface petProfileService

--- BackEnd/src/main/java/com/Group1/PetRadar/Service/PetProfileService.java
+++ BackEnd/src/main/java/com/Group1/PetRadar/Service/PetProfileService.java
@@ -1,8 +1,10 @@
 package com.Group1.PetRadar.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.Group1.PetRadar.DTO.pet.AddPetDTO;
+import com.Group1.PetRadar.Model.MedicalHistory;
 import com.Group1.PetRadar.Model.PetprofileModel;
 
 public interface PetProfileService {
@@ -12,6 +14,8 @@ public interface PetProfileService {
     PetprofileModel getPetprofileById(UUID id) throws Exception;
 
     PetprofileModel updatePetprofile(AddPetDTO petprofileModel);
+
+    List<MedicalHistory> getmedicalByPetId(UUID id) throws Exception;
 
     String deletePetprofileById(UUID id);
 
